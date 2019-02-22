a7d54f83e4a2a1e88ddce726702b2ea713d5379b
AtacanteRPT <rpaucara13@umsa.bo>
2019-02-22 15:27:17 -0400

    version 31

--- api/controllers/DatosController.js
+++ api/controllers/DatosController.js
@@ -89,8 +89,8 @@ module.exports = {
         console.log('ID ZONAA GLOBAL',id);
         pdfshift.convert('https://militantesmasipsp.com/datos/zonaPdf/'+ id, { "landscape": false, "use_print": false }).then(function (binary_file) {
             fs.writeFile(path.join(__dirname, '../.././assets/reportes/zona'+id+'.pdf'), binary_file, "binary", function () {
-
-                res.redirect('/reportes/zona'+id+'.pdf')
+                res.send('oso'+id);
+                //res.redirect('/reportes/zona'+id+'.pdf')
              })
         }).catch(function ({ message, code, response, errors = null }) { 
 
